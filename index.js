291de038a6a0896cb8d799f39a9924cbb62c6413
imi415 <imi415.public@gmail.com>
2016-02-02 10:31:50 +0900

    Finished OAuth

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var request = require('request');
 
-request({
-  url: 'http://www.pixiv.net',
-  headers: {
-    'User-Agent': 'Pixiv-Android/5.0.4'
-  }
-}, (error, response, body) => {
-  console.log(body);
-});
+oAuth('imi415.public@gmail.com', 'Sim14154');
+
+function oAuth(username, password){
+  var formData = {
+    client_id: 'BVO2E8vAAikgUBW8FYpi6amXOjQj',
+    client_secret: 'LI1WsFUDrrquaINOdarrJclCrkTtc3eojCOswlog',
+    grant_type: 'password',
+    username: username,
+    password: password,
+    device_token: 'pixiv',
+
+  };
+  request.post({
+    url: 'https://oauth.secure.pixiv.net/auth/token',
+    headers: {
+      'User-Agent': ''
+    },
+    formData: formData
+  }, (error, response, body) => {
+    console.log(body);
+  });
+}
